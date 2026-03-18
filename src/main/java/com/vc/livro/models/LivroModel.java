bc70419679e302c498490fbd3f04e2aeac301d9e
vinihcolombo <vinihcolombo@gmail.com>
2026-03-18 05:31:55 -0300

    Adição dos arquivos acidentalmente não adicionados no repositório

--- src/main/java/com/vc/livro/models/LivroModel.java
+++ src/main/java/com/vc/livro/models/LivroModel.java
@@ -1,4 +1,57 @@
 package com.vc.livro.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @Column(name = "Autor do Livro")
+    private String autor;
+
+    @Column(name = "Ano de Publicação")
+    private Integer anoPublicacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public LivroModel(){
+
+    }
+
+
 }
